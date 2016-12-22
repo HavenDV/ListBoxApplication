2cb351d96e40b0f749eea3035e1eaec1faaf285b
Konstantin S. <havendv@gmail.com>
2016-12-22 21:15:11 +1000

    Initial commit

--- ListBoxApplication/Form1.Designer.cs
+++ ListBoxApplication/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(12, 217);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(130, 32);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(142, 217);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(130, 32);
+            this.UpdateButton.TabIndex = 2;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button UpdateButton;
     }
 }
 
--- ListBoxApplication/Form1.cs
+++ ListBoxApplication/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,77 @@ namespace ListBoxApplication
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static void GoToTop(ListBox listBox)
+        {
+            listBox.TopIndex = 0;
+        }
+
+        public static void GoToBottom(ListBox listBox)
+        {
+            listBox.TopIndex = listBox.Items.Count - 1;
+        }
+
+        public static void Add(ListBox listBox, string text)
+        {
+            listBox.Items.Add(text);
+        }
+
+        public static void AddAndGoToTop(ListBox listBox, string text)
+        {
+            Add(listBox, text);
+            GoToTop(listBox);
+        }
+
+        public static void AddAndGoToBottom(ListBox listBox, string text)
+        {
+            Add(listBox, text);
+            GoToBottom(listBox);
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            AddAndGoToBottom(listBox1, $"randomText - {new Random().Next()}");
+        }
+
+        public static void Update(ListBox listBox, int index, string text)
+        {
+            //This animated way to change component
+            //listBox.Items[index] = text;
+
+            //This is not animated way to change component
+            var temp = listBox.SelectedIndex;
+            var objects = new object[listBox.Items.Count];
+            listBox.Items.CopyTo(objects, 0);
+            objects[index] = text;
+            listBox.Items.Clear();
+            listBox.Items.AddRange(objects);
+            listBox.SelectedIndex = temp;
+        }
+
+        public static void UpdateAndGoToTop(ListBox listBox, int index, string text)
+        {
+            Update(listBox, index, text);
+            GoToTop(listBox);
+        }
+
+        public static void UpdateAndGoToBottom(ListBox listBox, int index, string text)
+        {
+            Update(listBox, index, text);
+            GoToBottom(listBox);
+        }
+
+        public static void UpdateAndSavePosition(ListBox listBox, int index, string text)
+        {
+            int indexTemp = listBox.TopIndex;
+            Update(listBox, index, text);
+            listBox.TopIndex = indexTemp;
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            UpdateAndSavePosition(listBox1, listBox1.SelectedIndex, $"randomText - {new Random().Next()}");
         }
     }
 }
